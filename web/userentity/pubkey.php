513b9c70bce283d1a521588c87ce1fc7f509ef3c
Seriki Walter Yakub <sandraisom@icloud.com>
2026-07-02 04:03:51 +0100

    Add pubkey.php for user entity

--- web/userentity/pubkey.php
+++ web/userentity/pubkey.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * of the MIT license.  See the LICENSE file for details.
  */
 
-namespace Webauthn;
+namespace Webauth;
 
 use Assert\Assertion;
 use function Safe\base64_decode;
