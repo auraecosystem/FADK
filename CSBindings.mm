dc19f78e33fefc617207cbcfa0ba697fe5cd36c8
Seriki Walter Yakub <sandraisom@icloud.com>
2026-07-02 02:14:47 +0100

    Update and rename CSBindings.cpp to CSBindings.mm

--- CSBindings.mm
+++ CSBindings.mm
@@ -7,7 +7,7 @@
 //
 // See https://swift.org/LICENSE.txt for license information
 // See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
-//
+// 
 //===----------------------------------------------------------------------===//
 //
 // This file implements selection of bindings for type variables.
