eb4a6540dfe4a127722124a51bef6579d72efb02
Seriki Walter Yakub <webapp4@outlook.com>
2026-07-28 00:28:55 +0100

    Update erg.bat

--- erg.bat
+++ erg.bat
@@ -1,4 +1,3 @@
-
 setx GPU_MAX_HEAP_SIZE 100	
 setx GPU_MAX_USE_SYNC_OBJECTS 1	
 setx GPU_SINGLE_ALLOC_PERCENT 100	
@@ -11,5 +10,5 @@ setx GPU_MAX_WORKGROUP_SIZE 1024
 @cd /d "%~dp0"
 
 :: mine to herominers
-rigel.exe -a autolykos2 -o stratum+tcp://de.ergo.herominers.com:1180 -u 9ea4Jv2iHPSDv8xomrhByzgzscxE2oubyJkwJ9dyEGYxHjntNcd.331855 --log-file logs/miner.log
+rigel.exe -a autolykos2 -o stratum+tcp://de.ergo.herominers.com:1180 -u9ea4Jv2iHPSDv8xomrhByzgzscxE2oubyJkwJ9dyEGYxHjntNcd.331855 --log-file logs/miner.log
 pause
