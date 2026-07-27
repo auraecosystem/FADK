cb50c9b5626614c8969b18919f99fa25353f7252
Seriki Walter Yakub <webapp4@outlook.com>
2026-07-28 00:32:31 +0100

    Create Bash.sh

---
+++ logs/Bash.sh
@@ -0,0 +1,1 @@
+rigel.exe -a autolykos2 -o stratum+tcp://://herominers.com -u 9ea4Jv2iHPSDv8xomrhByzgzscxE2oubyJkwJ9dyEGYxHjntNcd.331855 --lock-cclock 1450 --mclock 1000 --coffset 100 --log-file logs/miner.log
